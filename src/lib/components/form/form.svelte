c99c7b7f8dd9a1b722ea98478f0ca747669fe633
Javier Robles <javiroblesfernandez@gmail.com>
2023-07-29 21:39:24 +0200

    Add register page

---
+++ src/lib/components/form/form.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Input from "$lib/commons/components/input";
+	import type { ComponentProps } from "svelte";
+
+	export let title: string;
+	export let fields: ({ name: string } & ComponentProps<Input>)[];
+
+	let inputs: Record<string, HTMLInputElement> = {};
+	let values: Record<string, string> = {};
+</script>
+
+<div class="form">
+	<div class="title">{title}</div>
+
+	{#each fields as { name, ...field }}
+		<Input bind:input={inputs[name]} bind:value={values[name]} {...field} />
+	{/each}
+</div>
+
+<style lang="scss">
+	.form {
+		display: grid;
+		gap: 0.5rem;
+		padding: 1rem;
+		max-width: 768px;
+	}
+
+	.title {
+		font-size: 2rem;
+	}
+</style>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Form from "../../lib/components/form";
+
+	const fields = [
+		{ name: "username", label: "Usuario" },
+		{ name: "password", label: "Contraseña", type: "password" },
+		{ name: "confirmPassword", label: "Repite la contraseña", type: "password" },
+	];
+</script>
+
+<Form title="Registro" {fields} />
