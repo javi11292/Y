e2f9098ba1cf476972902bdc83c130f97fa7fcea
Javier Robles <javiroblesfernandez@gmail.com>
2023-08-20 14:56:47 +0200

    Redirect with no session

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,2 +1,17 @@
-export const trailingSlash = "always";
+import { redirect } from "@sveltejs/kit";
+
 export const prerender = true;
+
+export const load = async ({ fetch, url }) => {
+	const response = await fetch("/api/user");
+
+	if (!response.ok && url.pathname !== "/login" && url.pathname !== "/register") {
+		throw redirect(307, "/login");
+	}
+
+	if (response.ok && (url.pathname === "/login" || url.pathname === "/register")) {
+		throw redirect(307, "/");
+	}
+
+	return response.json();
+};
