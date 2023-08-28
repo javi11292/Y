253438263851a5d92c6e54c1a09c7b8e1ec9793a
Javier Robles <javiroblesfernandez@gmail.com>
2023-08-28 23:19:46 +0200

    Add like button

--- src/routes/tweet.svelte
+++ src/routes/tweet.svelte
@@ -1,6 +1,7 @@
 <svelte:options immutable />
 
 <script lang="ts">
+	import Icon from "$lib/commons/components/icon";
 	import type { Post } from "$lib/models/post";
 
 	export let observer: IntersectionObserver;
@@ -20,10 +21,6 @@
 	};
 
 	const getDate = (iso: Date) => {
-		if (!iso) {
-			return "";
-		}
-
 		const date = new Date(iso);
 		let diff = Math.floor((Date.now() - date.getTime()) / 1000);
 
@@ -53,14 +50,25 @@
 		<span class="date">{getDate(post.date)}</span>
 	</div>
 	{post.content}
+	<div class="stats">
+		<span class="button">
+			<span class="icon">
+				<Icon icon="favoriteBorder" />
+			</span>
+			<span class="value">
+				{post.likes}
+			</span>
+		</span>
+	</div>
 </div>
 
 <style lang="scss">
 	@use "$lib/commons/theme";
+	@use "$lib/commons/classes";
 
 	.post {
 		border-bottom: 1px solid theme.$colorNeutralDark;
-		padding: 1rem;
+		padding: 1rem 1rem 0;
 	}
 
 	.meta {
@@ -74,5 +82,50 @@
 	.date {
 		margin-left: 0.5rem;
 		color: theme.$colorNeutral;
+	}
+
+	.stats {
+		margin: 0.25rem 0;
+		color: theme.$colorNeutral;
+		display: flex;
+	}
+
+	.value {
+		transition: all;
+		transition-duration: 200ms;
+	}
+
+	.icon {
+		@extend %root;
+		border-radius: 50%;
+		overflow: hidden;
+		padding: 0.5rem;
+		margin-left: -0.5rem;
+		font-size: 1.5rem;
+	}
+
+	.button {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+		cursor: pointer;
+
+		@media (hover: hover) {
+			&:hover {
+				color: theme.$colorPrimary;
+
+				.icon {
+					@include classes.hover;
+				}
+			}
+		}
+
+		&:active {
+			color: theme.$colorPrimary;
+
+			.icon {
+				@include classes.active;
+			}
+		}
 	}
 </style>
