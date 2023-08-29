5f1dfd5f1936cc9c6a55e1dc9f3fd1329c6a3952
Javier Robles <javiroblesfernandez@gmail.com>
2023-08-29 15:02:47 +0200

    Add loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
+	import LoadingIcon from "$lib/commons/components/loading-icon";
 	import { get } from "$lib/commons/utils/fetch";
 	import Post from "$lib/components/post";
 	import type { PostId } from "$lib/models/post";
+	import { fade } from "svelte/transition";
 	import Tweet from "./tweet.svelte";
 
 	export let data;
 
 	let posts: PostId[] = [];
 	let lastId = "";
+	let loading = false;
 
 	$: {
 		data.streamed.posts.then((value) => (posts = value));
@@ -18,9 +21,11 @@
 		const lastPost = posts[posts.length - 1];
 
 		if (lastPost && lastId !== lastPost._id) {
+			loading = true;
 			posts.push(...((await get(`/api/post/${lastPost._id}`)) as PostId[]));
 			posts = posts;
 			lastId = lastPost._id;
+			loading = false;
 		}
 	};
 
@@ -40,3 +45,17 @@
 {#each posts as post, index}
 	<Tweet {post} {observer} observe={index + 1 === posts.length} />
 {/each}
+
+{#if loading}
+	<div in:fade class="loading">
+		<LoadingIcon />
+	</div>
+{/if}
+
+<style lang="scss">
+	.loading {
+		display: flex;
+		justify-content: center;
+		padding: 2rem;
+	}
+</style>
