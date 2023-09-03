f73805547957498180564b85ccc51f492aa7dbea
Javier Robles <javiroblesfernandez@gmail.com>
2023-09-04 00:42:40 +0200

    Add tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Button from "$lib/commons/components/button";
 	import Header from "$lib/components/header";
 	import Post from "$lib/components/post";
-	import { fly } from "svelte/transition";
+	import { crossfade, fly } from "svelte/transition";
 	import Tab1 from "./tab1.svelte";
 
 	enum tabs {
@@ -20,8 +20,11 @@
 		active = tab;
 	};
 
+	const key = { key: "indicator" };
 	let active = tabs.tab1;
 	let direction = 1;
+
+	const [send, receive] = crossfade({ duration: 200 });
 </script>
 
 <Post />
@@ -31,14 +34,20 @@
 		<div class="tabs">
 			<Button disableBorder disableActive disableUpperCase on:click={() => handleClick(tabs.tab1)}>
 				Para ti
+				{#if active === tabs.tab1}
+					<div class="indicator" in:receive={key} out:send={key} />
+				{/if}
 			</Button>
 			<Button disableBorder disableActive disableUpperCase on:click={() => handleClick(tabs.tab2)}>
 				Siguiendo
+				{#if active === tabs.tab2}
+					<div class="indicator" in:receive={key} out:send={key} />
+				{/if}
 			</Button>
 		</div>
 	</Header>
 
-	<div class="tab">
+	<div class="content">
 		{#key active}
 			<div
 				in:fly={{ x: `${100 * direction}%`, opacity: 1 }}
@@ -62,14 +71,24 @@
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
 		border-bottom: 1px solid theme.$colorNeutralDark;
+		position: relative;
 	}
 
-	.tab {
+	.content {
 		display: grid;
 		grid-template-areas: "content";
 
 		& > * {
 			grid-area: content;
 		}
+	}
+
+	.indicator {
+		position: absolute;
+		bottom: 0;
+		translate: -50%;
+		left: 50%;
+		width: 30%;
+		border-bottom: 5px solid theme.$colorPrimary;
 	}
 </style>
