f980ad9b4e7f0b03c1a6d755a58ba77c4d4d42c2
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-10 19:17:36 +0100

    Change to old cache

--- astro.config.js
+++ astro.config.js
@@ -12,7 +12,7 @@ const workbox = {
 			skipWaiting: true,
 			runtimeCaching: [
 				{
-					urlPattern: ({ request }) => !request.headers.has("Referer"),
+					urlPattern: ({ url }) => !/^\/_astro/.test(url.pathname),
 					handler: "NetworkFirst",
 					options: {
 						cacheName: "Astro",
