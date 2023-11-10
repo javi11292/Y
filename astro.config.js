8ede2d5b25161cf8cda4b2fd09d2584c68f89241
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-10 11:38:50 +0100

    Readd cache

--- astro.config.js
+++ astro.config.js
@@ -12,6 +12,13 @@ const workbox = {
 			skipWaiting: true,
 			runtimeCaching: [
 				{
+					urlPattern: ({ url }) => /^\/_astro/.test(url.pathname),
+					handler: "CacheFirst",
+					options: {
+						cacheName: "Astro",
+					},
+				},
+				{
 					urlPattern: ({ url }) => !/^\/_astro/.test(url.pathname),
 					handler: "NetworkFirst",
 					options: {
