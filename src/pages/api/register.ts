8cae122af420f4f5476ccd67ecc5afd876f818cf
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-12 14:14:52 +0100

    Typo

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -36,7 +36,7 @@ export const POST = async ({ request, locals }: AstroGlobal) => {
 	}
 
 	if (!/^\w+$/.test(name)) {
-		return errorResponse("El nombre solo puede contener caracteres alfanuméricos", 400);
+		return errorResponse("El nombre sólo puede contener caracteres alfanuméricos", 400);
 	}
 
 	const { error, data } = await locals.supabase.auth.signUp({ email, password });
