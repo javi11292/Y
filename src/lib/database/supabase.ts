d113a3edc92b53b58adafc56af302e66dd8c77ba
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-20 14:18:39 +0100

    Change cloudflare version

--- src/lib/database/supabase.ts
+++ src/lib/database/supabase.ts
@@ -1,17 +1,13 @@
 import type { Database } from "$lib/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-let client;
-
-try {
-	client = createClient<Database>(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ADMIN, {
+export const supabase = createClient<Database>(
+	import.meta.env.SUPABASE_URL,
+	import.meta.env.SUPABASE_ADMIN,
+	{
 		auth: {
 			autoRefreshToken: false,
 			persistSession: false,
 		},
-	});
-} catch {
-	throw new Error("LALA" + import.meta.env.SUPABASE_URL);
-}
-
-export const supabase = client;
+	},
+);
