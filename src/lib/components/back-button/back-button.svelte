1b45e8091e74a36be576460cc812d3437947f9d6
Javier Robles <javiroblesfernandez@gmail.com>
2023-11-26 20:12:52 +0100

    Add back button

---
+++ src/lib/components/back-button/back-button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Button from "$lib/commons/components/button";
+
+	const handleClick = () => {
+		history.back();
+	};
+</script>
+
+<Button icon="arrow-right" mirror onclick={handleClick} />
